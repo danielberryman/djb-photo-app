ebe89d248023df824e919ec46d3cdf2dac120374
Daniel Berryman <danielberryman90@gmail.com>
2018-05-03 18:20:07 -0400

    Fixed env var bug, updated user/payment models for associations and methods, updated registration view page with credit card info fields

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 #the following comes directly from the stripe documentation
 
 Rails.configuration.stripe = {
-	:publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY']
+	:publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
 	:secret_key => ENV['STRIPE_TEST_SECRET_KEY']
 }
 
